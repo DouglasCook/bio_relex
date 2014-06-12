68e549aba70a1bb68ce70a26736e54e2b6e8419a
Douglas Cook <douglas.cook13@imperial.ac.uk>
2014-06-12 10:35:45 +0100

    Lets get started

---
+++ nltk_shizzle/basics.py
@@ -0,0 +1,73 @@
+""" This module will be used to record essential tidbits on how to use NLTK """
+import nltk
+
+#from nltk.corpus import webtext      # webtext is web stuff
+#from nltk.corpus import brown        # Brown corpus contains 500 texts from various genres
+#from nltk.corpus import reuters      # includes training and test sets from news documents
+
+
+def gutenberg():
+    """ Gutenberg contains books 
+        This shows some of the annotations included in the NLTK corpora """
+    from nltk.corpus import gutenberg 
+
+    # loop through all files in corpus
+    for fileid in gutenberg.fileids():
+        # raw treats whole text as char string 
+        num_chars = len(gutenberg.raw(fileid))
+        # words and sents depend on the corpus being annotated 
+        num_words = len(gutenberg.words(fileid))
+        num_sents = len(gutenberg.sents(fileid))
+        num_vocab = len(set([w.lower for w in gutenberg.words(fileid)]))
+
+        print int(num_chars/num_words), int(num_words/num_sents), int(num_words/num_vocab), fileid
+
+
+def inaugural():
+    """ Inaugural speeches given by presidents 
+        This calculates a conditional frequency distribution """
+    from nltk.corpus import inaugural
+
+    # plots a conditional frequency distribution
+    cfd = nltk.ConditionalFreqDist(
+            # this dictates what is plotted and the x axis
+            (target, fileid[:4])
+            for fileid in inaugural.fileids()
+            for w in inaugural.words(fileid)
+            for target in ['america', 'citizen']
+            if w.lower().startswith(target))
+
+    # show results in a table
+    cfd.tabulate()
+    # or graph
+    cfd.plot()
+
+
+def own_corpora():
+    """ How to use your own corpora """
+    # import plaintext utilities
+    from nltk.corpus import PlaintextCorpusReader
+
+    # provide the root of directory for corpus
+    corpus_root = 'DDFsamples'
+    wordlists = PlaintextCorpusReader(corpus_root, '.*')
+
+    print 'Files are', wordlists.fileids()
+
+
+def names():
+    names = nltk.corpus.names
+    males = names.words('male.txt')
+    females = names.words('female.txt')
+
+    print len(males), 'male and', len(females), 'female names'
+
+    cfd = nltk.ConditionalFreqDist(
+            (f, n[0])
+            for f in names.fileids()
+            for n in names.words(f)) 
+
+    cfd.plot()
+
+if __name__ == '__main__':
+    names()
