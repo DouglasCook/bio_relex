06d12cb17a45a08dcdcd560a7efa909b6c576f5c
Douglas Cook <douglas.cook13@imperial.ac.uk>
2014-07-07 21:18:40 +0100

    splitting eu-adr text into sentences

--- eu_adr/preprocess.py
+++ eu_adr/preprocess.py
@@ -2,6 +2,21 @@ import os
 import csv
 import xml.etree.cElementTree as ET     # python XML manipulation library, C version because it's way faster!
 
+import nltk.tokenize.punkt as punkt
+
+
+def set_up_tokenizer():
+    """
+    Set up sentence splitter with custom parameters and return to caller
+    """
+    punkt_params = punkt.PunktParameters()
+    # sentences are not split ending on the given parameters, using {} creates a set literal
+    punkt_params.abbrev_types = {'inc', '.tm', 'tm', 'no', 'i.v', 'dr', 'drs', 'u.s', 'u.k', 'ltd', 'vs', 'vol', 'corp',
+                                 'jan', 'feb', 'mar', 'apr', 'jun', 'jul', 'aug', 'sep', 'sept', 'oct', 'nov', 'dec',
+                                 'pm', 'p.m', 'am', 'a.m', 'mr', 'mrs', 'ms', 'i.e'}
+    # the tokenizer has to be unpickled so better do it once here than every time it is used
+    return punkt.PunktSentenceTokenizer(punkt_params)
+
 
 def convert_to_csv():
     """
@@ -16,8 +31,10 @@ def convert_to_csv():
     files = [os.path.abspath(os.path.join(basepath, 'abstracts', f)) for f in files if f != '.DS_Store']
     f_out = os.path.abspath(os.path.join(basepath, 'csv', 'texts.csv'))
 
+    sentence_splitter = set_up_tokenizer()
+
     with open(f_out, 'wb') as csv_out:
-        csv_writer = csv.DictWriter(csv_out, ['id', 'text'], delimiter=',')
+        csv_writer = csv.DictWriter(csv_out, ['id', 'sent_num', 'text'], delimiter=',')
         csv_writer.writeheader()
 
         for f in files:
@@ -28,11 +45,15 @@ def convert_to_csv():
             pubmed_id = tree.findtext('.//PMID')
             title = tree.findtext('.//ArticleTitle')
             abstract = ' '.join([e.text for e in tree.findall('.//AbstractText')])
+            text = title + ' ' + abstract
 
-            # dict comprehension here to hack the unicode into csv writer
-            dict_row = {'id': pubmed_id, 'text': title + ' ' + abstract}
-            row = {key: value.encode('utf-8') for key, value in dict_row.items()}
-            csv_writer.writerow(row)
+            sentences = sentence_splitter.tokenize(text)
+
+            for i, s in enumerate(sentences):
+                # dict comprehension here to hack the unicode into csv writer
+                dict_row = {'id': pubmed_id, 'sent_num': i, 'text': s.encode('utf-8')}
+                csv_writer.writerow(dict_row)
+
 
 if __name__ == '__main__':
     convert_to_csv()
